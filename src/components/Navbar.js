313f3ea3ddb2518bbf6ba6ad0617a517ec8fc1ed
yuralala8 <yc1435@nyu.edu>
2017-11-15 13:06:56 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import { NavLink, Redirect } from 'react-router-dom';
+
+const Navbar = () => {
+	return(
+		<div>
+		<p> Hello</p>
+		</div>
+		)
+}
+
+export default Navbar
