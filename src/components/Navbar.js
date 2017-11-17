1c9f49283b63e9ac4d11163c85a852fdc4ec930a
yuralala8 <yc1435@nyu.edu>
2017-11-17 01:04:00 -0500

    styled menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,20 @@ import { NavLink, Redirect } from 'react-router-dom';
 
 const Navbar = () => {
 	return(
-		<div>
-		<p> Hello</p>
+		<div className="navbar">
+			<div className="row">
+			<img src="/buzzfeed.png" className="logo"/>
+				<div className="menu">
+					<li className="menu-item">News</li>
+					<li className="menu-item">Videos</li>
+					<li className="menu-item">Quizzes</li>
+					<li className="menu-item">Tasty</li>
+					<li className="menu-item">More <i class="fa fa-caret-down" aria-hidden="true"></i></li>
+				</div>
+				<div className="search-box">
+					<input type="text" placeholder="Search" className="search"/>
+				</div>
+			</div>
 		</div>
 		)
 }
